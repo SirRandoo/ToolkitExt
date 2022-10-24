78213f1a3ccdbabf847620bd417a6ef6d601ded8
sirrandoo <8146689+SirRandoo@users.noreply.github.com>
2022-10-24 15:47:50 -0500

    Fixed a gamma issue when creating gif frames.

--- Source/ToolkitExt.Core/Extensions/TextureExtensions.cs
+++ Source/ToolkitExt.Core/Extensions/TextureExtensions.cs
@@ -22,6 +22,7 @@
 
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
 namespace ToolkitExt.Core.Extensions
 {
@@ -30,11 +31,11 @@ namespace ToolkitExt.Core.Extensions
         [NotNull]
         public static Texture2D CopyFromReadOnly([NotNull] this Texture2D original)
         {
-            RenderTexture tmp = RenderTexture.GetTemporary(original.width, original.height, 0, RenderTextureFormat.Default, RenderTextureReadWrite.Linear);
+            RenderTexture tmp = RenderTexture.GetTemporary(original.width, original.height);
             Graphics.Blit(original, tmp);
             RenderTexture previous = RenderTexture.active;
             RenderTexture.active = tmp;
-            var tex = new Texture2D(original.width, original.height);
+            var tex = new Texture2D(original.width, original.height, tmp.graphicsFormat, TextureCreationFlags.None);
             tex.ReadPixels(new Rect(0, 0, tmp.width, tmp.height), 0, 0);
             tex.Apply();
 
@@ -47,7 +48,7 @@ namespace ToolkitExt.Core.Extensions
         [NotNull]
         public static Texture2D GetFrameFromSheet([NotNull] this Texture2D sheet, int index)
         {
-            var texture = new Texture2D(sheet.height, sheet.height);
+            var texture = new Texture2D(sheet.height, sheet.height, sheet.format, false, true);
             texture.SetPixels(sheet.GetPixels(index * texture.height, 0, texture.height, texture.height));
             texture.Apply();
 
@@ -62,7 +63,7 @@ namespace ToolkitExt.Core.Extensions
             
             for (var i = 0; i < totalFrames; i++)
             {
-                var texture = new Texture2D(sheet.height, sheet.height);
+                var texture = new Texture2D(sheet.height, sheet.height, sheet.format, false, true);
                 texture.SetPixels(sheet.GetPixels(i * texture.height, 0, texture.height, texture.height));
                 texture.Apply();
 
